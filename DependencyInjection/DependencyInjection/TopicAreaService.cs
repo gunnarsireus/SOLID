7909c6292df32ab2431576b6940fe59154a60c25
KONGUNS <KONGUNS@CLN10907>
2018-01-08 07:11:23 +0100

    Added project

---
+++ DependencyInjection/DependencyInjection/TopicAreaService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public class TopicAreaService:ITopicAreaService
+    {
+	    public IEnumerable<TopicArea> GetAllTopicAreas()
+	    {
+		    return new List<TopicArea>
+		    {
+			    new TopicArea {Name =".NET Core" },
+			    new TopicArea {Name ="Docker" },
+			    new TopicArea { Name ="C#" }
+		    };
+	    }
+	}
+}
